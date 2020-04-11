978ae98721b85ebe05acd6314d248aeb5fba5c2b
Mohammad Shakil <60793722+shamsuddin-alam@users.noreply.github.com>
2020-04-11 20:46:45 +0600

    Added new files

---
+++ index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Its specially ladies comfort zone.Basically its a ladies perlar.Where you can achieve various home service. If u wish secretly or You want in broad day light">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Rufina:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/uikit.min.css">
+    <link rel="stylesheet" href="css/navbar-fixed.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Leospa - Clean & Minimal SCSS HTML5 Template</title>
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+</head>
+
+<body>
+    <header id="header">
+        <nav class="navbar navbar-expand-md" uk-sticky="top: 200; animation: uk-animation-slide-top; bottom: #sticky-on-scroll-up">
+            <div class="container position">
+                <a href="#header">
+                    <img src="img/logo.png" alt="SPA-LOGO">
+                </a>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarnav"><span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span></button>
+                <div class="collapse navbar-collapse" id="navbarnav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item mt-sm-0 mt-md-5 pt-sm-2 pt-md-0 pr-sm-0 pr-md-4 active"><a class="nav-link" href="#header">HOME</a></li>
+                        <li class="nav-item mt-sm-0 mt-md-5 pr-sm-0 pr-md-4"><a class="nav-link" href="#about">ABOUT</a></li>
+                        <li class="nav-item mt-sm-0 mt-md-5 pr-sm-0 pr-md-4"><a class="nav-link" href="#service">SERVICE</a></li>
+                        <li class="nav-item mt-sm-0 mt-md-5 pr-sm-0 pr-md-4"><a class="nav-link" href="#team">TEAM</a></li>
+                        
+                        <li class="nav-item mt-sm-0 mt-md-5"><a class="nav-link" href="#appointment">CONTACT</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- Banner Section -->
+    <section id="banner" class="banner">
+        <div class="spa-image d-none d-md-block">
+            <img class="img-fluid" src="img/spa.png" alt="Spa-image">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-9 col-lg-6">
+                    <div class="banner-content" uk-lightbox>
+                        <span class="d-inline-block pb-4">spa & beauty center</span>
+                        <h1 class="display-4">Beauty and success starts here.</h1>
+                        <p class="py-3">Together creeping heaven upon third dominion be upon won't darkness rule behold it created good saw after she'd Our set living.</p>
+                        <button class="btn button">Reserve Now</button>
+                        <div class="d-inline-block">
+                            <a class="px-4" href="https://youtu.be/Udqui_kPVNU"><i class="fa fa-play pr-4"></i>
+                                Watch our story</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- About Section -->
+    <section id="about" class="py-5">
+        <img class="china-rose d-sm-inline-block d-none" src="img/china-rose.png" alt="China-rose">
+        <img class="jasmine d-lg-inline-block d-none" src="img/jasmine.png" alt="Jasmine">
+        <div class="container">
+            <div class="col-lg-10 col-md-12 text-center mx-auto">
+                <img src="img/butterfly.png" alt="Butterfly">
+                <div class="about-text">
+                    <span class="text-title d-inline-block py-4 text-uppercase">
+                        About our spa center
+                    </span>
+                    <h2 class="heading">Come and you will be Inspired!</h2>
+                    <p class="description py-4 text-muted">It’s the end of summer the sweltering heat makes human sweat in the night and makes the plants and trees wilt even in the moonlit nights. The eastern wind breeze brings an eerie feeling, that the monsoon clouds are soon coming, there is a strange silence in the ears, the sky gets darker and darker</p>
+                    <a class="btn button about-btn" href="">Read More</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Service Section -->
+    <section id="service" class="py-5 my-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-12 service-item">
+                    <div class="service-image">
+                        <img class="img-fluid w-100" src="img/service/1.jpg" alt="">
+                    </div>
+                    <div class="service-content">
+                        <img src="img/icons/1.png" alt="">
+                        <p class="pt-4">Body Treatment</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-12 service-item">
+                    <div class="service-image">
+                        <img class="img-fluid w-100" src="img/service/2.jpg" alt="">
+                    </div>
+                    <div class="service-content">
+                        <img src="img/icons/1.png" alt="">
+                        <p class="pt-4">Body Treatment</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-12 service-item">
+                    <div class="service-image">
+                        <img class="img-fluid w-100" src="img/service/3.jpg" alt="">
+                    </div>
+                    <div class="service-content">
+                        <img src="img/icons/1.png" alt="">
+                        <p class="pt-4">Body Treatment</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-12 service-item">
+                    <div class="service-image">
+                        <img class="img-fluid w-100" src="img/service/4.jpg" alt="">
+                    </div>
+                    <div class="service-content">
+                        <img src="img/icons/1.png" alt="">
+                        <p class="pt-4">Body Treatment</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Procedures -->
+    <section id="procedures" class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h2>Popular Procedures</h2>
+                    <p class="py-4">To doesn't his appear replenish together called he of mad place won't wherein blessed second every wherein were meat kind wherein and martcin</p>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-lg-4 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <img class="img-fluid" src="img/post/1.jpg" alt="">
+                            <h4 class="py-3">Massage Therapy</h4>
+                            <p>Living winged said you darkness you're divide gathered and bring one seasons face great dr Waters firmament place which.</p>
+                            <a class="btn button pd-btn my-3" href="#">READ MORE</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <img class="img-fluid" src="img/post/2.jpg" alt="">
+                            <h4 class="py-3">Beauty Care</h4>
+                            <p>Living winged said you darkness you're divide gathered and bring one seasons face great dr Waters firmament place which.</p>
+                            <a class="btn button pd-btn my-3" href="#">READ MORE</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <img class="img-fluid" src="img/post/3.jpg" alt="">
+                            <h4 class="py-3">Executive Reflexology</h4>
+                            <p>Living winged said you darkness you're divide gathered and bring one seasons face great dr Waters firmament place which.</p>
+                            <a class="btn button pd-btn my-3" href="#">READ MORE</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Testimonial -->
+    <section id="testimonial">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div id="mycarousel" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-slide-to="0" data-target="#mycarousel" class="active"></li>
+                            <li data-slide-to="1" data-target="#mycarousel"></li>
+                        </ol>
+                        <div class="carousel-inner text-center">
+                            <div class="carousel-item active">
+                                <img src="img/quote.png" alt="">
+                                <p class="py-4">First i beast be fruitful open you tree all Won't can't likeness and you're have whales creature seed to two grass life blessed you meat shall you winged under from their there he That you're one called gather make much red wherein set fourth green bearing fifth replenish given she had.</p>
+                                <div class="client-info">
+                                    <img class="img-fluid rounded-circle" src="img/client-1.png" alt="Client">
+                                    <p class="py-4">Jack Marsh, <span>Executive</span></p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="img/quote.png" alt="">
+                                <p class="py-4">First i beast be fruitful open you tree all Won't can't likeness and you're have whales creature seed to two grass life blessed you meat shall you winged under from their there he That you're one called gather make much red wherein set fourth green bearing fifth replenish given she had.</p>
+                                <div class="client-info">
+                                    <img class="img-fluid rounded-circle client-2" src="img/client-2.jpg" alt="Client">
+                                    <p class="py-4">Jonson Hitler, <span>Executive</span></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Team Section -->
+    <section id="team" class="py-5 text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Experienced Team</h2>
+                    <p class="py-3">To doesn't his appear replenish together called he of mad place won't wherein blessed second every wherein were meat kind wherein and martcin</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-12">
+                    <div class="team-member">
+                        <img class="img-fluid" src="img/team/1.jpg" alt="">
+                        <div class="member-details">
+                            <h4>Joseph Austin</h4>
+                            <p>Thai Message</p>
+                            <div class="social-link">
+                                <a href="#"><i class="fa fa-facebook-f"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-google-plus"></i></a>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="team-member">
+                        <img class="img-fluid" src="img/team/2.jpg" alt="">
+                        <div class="member-details">
+                            <h4>David Hannan</h4>
+                            <p>Thai Message</p>
+                            <div class="social-link">
+                                <a href="#"><i class="fa fa-facebook-f"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-google-plus"></i></a>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="team-member">
+                        <img class="img-fluid" src="img/team/3.jpg" alt="">
+                        <div class="member-details">
+                            <h4>Cheryl Harris</h4>
+                            <p>Thai Message</p>
+                            <div class="social-link">
+                                <a href="#"><i class="fa fa-facebook-f"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-google-plus"></i></a>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Appointment Section -->
+    <section id="appointment" class="py-5">
+        <div class="row">
+            <div class="col-lg-5 col-12 d-sm-block d-none">
+                <div class="appoit-img">
+                    <img class="img-fluid w-100" src="img/appointment-img.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-lg-7 col-12 py-4 bg">
+                <div class="container">
+                    <form action="https://formspree.io/maydpwqz" method="post">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <input class="form-control mb-4" type="text" name="NAME" placeholder="NAME"
+                                    required>
+                                </div>
+                                <div class="form-group select-field">
+                                    <select class="form-control text-uppercase mb-4" name="SERVICE-NO" id="" required>
+                                        <option value="Select Service">Select Service</option>
+                                        <option value="Service-1">Service-1</option>
+                                        <option value="Service-2">Service-2</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <input name="DATE" class="form-control mb-4" type="date">
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <input name="EMAIL" class="form-control mb-4" type="email" placeholder="EMAIL ADDRESS" required>
+                                </div>
+                                <div class="form-group">
+                                    <input name="PHONE-NUMBER" class="form-control mb-4" type="tel" placeholder="PHONE NUMBER" required>
+                                </div>
+                                <div class="form-group">
+                                    <input name="TIME" class="form-control mb-4" type="time">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <textarea class="form-control mb-4" name="MESSAGE" id="" rows="5" placeholder="YOUR MESSAGE"></textarea>
+                                </div>
+
+                            </div>
+                            <div class="col-12 text-center">
+                                <div class="form-group">
+                                    <button class="btn button  btn-ap" type="submit">MAKE AN APPOINTMENT</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <footer id="footer" class="py-5 text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <img src="img/logo.png" alt="">
+                    <div class="social-icon py-4">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-vimeo"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                    </div>
+                    <p><span>&copy; COPYRIGHT 2020 <a href="https://www.facebook.com/mohaammad.shakil/" target="_blank">Shakil.</a> ALL RIGHTS RESERVED.</span></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://kit.fontawesome.com/2af9d03f1f.js" crossorigin="anonymous"></script>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <script src="js/selectivizr-min.js"></script>
+    <script src="js/navbar-fixed.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
